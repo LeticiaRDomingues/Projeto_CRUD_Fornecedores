9ec3285011d2b91c0001efa309a5314163964fd8
LeticiaRDomingues <leticiardominguescontato@gmail.com>
2023-05-30 11:15:54 -0300

    commit final

--- Projeto_Fornecedores/src/app/app.module.ts
+++ Projeto_Fornecedores/src/app/app.module.ts
@@ -4,15 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { SupplierComponent } from './supplier/supplier.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SupplierComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    HttpClientModule,
+    FormsModule,
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
